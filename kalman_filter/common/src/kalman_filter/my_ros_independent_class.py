20ac49a13b7af038fef9c1e599d0afa81db0c3e7
pedrofcsousa <pedrofcsousa@tecnico.ulisboa.pt>
2017-11-20 14:56:17 +0000

    5th floor map working

--- kalman_filter/common/src/kalman_filter/my_ros_independent_class.py
+++ kalman_filter/common/src/kalman_filter/my_ros_independent_class.py
@@ -3,7 +3,7 @@
 import numpy as np
 import math
 
-N_ARUCOS=16
+N_ARUCOS=29
 
 class ArucoInfo():
 	def __init__(self, arucoId, arucox, arucoy, arucoAlfa, arucox_world, arucoy_world, arucoAlfa_world):
